53ac53e131ba0800816e3954e27ca7eadb98fc10
douglasmatosdev <douglasmatosdev@gmail.com>
2022-12-18 13:13:35 -0300

    First App

---
+++ FirstApp/app/src/main/java/com/douglasmatosdev/firstapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.douglasmatosdev.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void alterarTexto(View view) {
+        TextView texto = findViewById(R.id.textoExibicao);
+        texto.setText("Douglas Matos da Silva");
+    }
+}
